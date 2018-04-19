9045130595a7c80f81958827a0998d10a1ac83d3
gutfeeling <dibyachakravorty@gmail.com>
2018-04-19 13:34:44 +0200

    add missing requirements

--- 10_proximal_policy_optimization/reference_implementation/setup.py
+++ 10_proximal_policy_optimization/reference_implementation/setup.py
@@ -11,7 +11,7 @@ setup(name = "ppo",
       author = "Dibya Chakravorty",
       author_email = "dibyachakravorty@gmail.com",
       install_requires=[
-          "argparse-1.4.0",
+          "argparse==1.4.0",
           # There's a bug in Box2D environments like LunarLander-v2 or
           # BipedalWalker-v2 (https://github.com/openai/gym/issues/100).
           # Fix it by running the following after this package has been
@@ -20,6 +20,7 @@ setup(name = "ppo",
           # git clone https://github.com/pybox2d/pybox2d.git
           # pip install -e pybox2d
           "gym[all]==0.10.5",
+          "h5py==2.7.1",
           "Keras==2.1.5",
           "scipy==1.0.1",
           # Assuming Tensorflow backend (CPU version) for Keras
